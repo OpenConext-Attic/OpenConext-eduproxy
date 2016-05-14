10a8f3583ab995a5997c2875386d48414e46633f
oharsta <oharsta@zilverline.com>
2016-05-14 11:25:49 +0200

    Deleted sql test files

--- src/test/resources/sql/seed.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `users` (`unspecified_id`, `username`, `email`, `central_idp`, `mapped`, `confirmed`, `affiliations`, `invite_hash`, `institution`)
-VALUES
-	('urn:collab:person:idin.nl:confirmed', 'J. Doe', 'jdoe@example.com', 'https://idin.surfconext.nl/saml2/idp/metadata.php', 1, 1, 'researcher, student', 'http://localhost:8080/confirmation?inviteHash=hash', 'example.com');
-
-INSERT INTO `users` (`unspecified_id`, `username`, `email`, `central_idp`, `mapped`, `confirmed`, `affiliations`, `invite_hash`, `institution`)
-VALUES
-	('urn:collab:person:idin.nl:mapped', 'J. Doe', 'jdoe@example.com', 'https://idin.surfconext.nl/saml2/idp/metadata.php', 1, 0, 'researcher, student', 'http://localhost:8080/confirmation?inviteHash=hash', 'example.com');
